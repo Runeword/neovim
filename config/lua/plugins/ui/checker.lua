cbf6a4ea815499a9ab3474fea02faabf8676c96f
Runeword <60324746+Runeword@users.noreply.github.com>
2025-09-08 21:03:38 +0200

    Update config/lua/plugins/ui/checker.lua

--- config/lua/plugins/ui/checker.lua
+++ config/lua/plugins/ui/checker.lua
@@ -8,8 +8,8 @@ return {
   dir = vim.fn.stdpath('config') .. '/lua/myplugins/checker.nvim',
 
   config = function()
-    vim.keymap.set('n', '<S-PageUp>', require('checker').prevDiagnostic, { noremap = true, silent = true, })
-    vim.keymap.set('n', '<S-PageDown>', require('checker').nextDiagnostic, { noremap = true, silent = true, })
+    vim.keymap.set('n', '<PageUp>', require('checker').prevDiagnostic, { noremap = true, silent = true, })
+    vim.keymap.set('n', '<PageDown>', require('checker').nextDiagnostic, { noremap = true, silent = true, })
 
     -- vim.keymap.set('n', '<PageUp>', vim.diagnostic.goto_prev, { buffer = buffer, })
     -- vim.keymap.set('n', '<PageDown>', vim.diagnostic.goto_next, { buffer = buffer, })
