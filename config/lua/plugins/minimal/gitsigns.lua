3e29898998c340c6cbcc345274a8cb703675a101
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-08 23:29:26 +0100

    Update config/lua/plugins/minimal/gitsigns.lua

--- config/lua/plugins/minimal/gitsigns.lua
+++ config/lua/plugins/minimal/gitsigns.lua
@@ -2,71 +2,48 @@ local vim = vim
 
 return {
   'lewis6991/gitsigns.nvim',
-
   enabled = true,
 
   config = function()
     require('gitsigns').setup({
+
+      signs_staged_enable = true,
       current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 200,
-      },
+      current_line_blame_opts = { delay = 200, },
 
       on_attach = function(buffer)
-        vim.keymap.set({ 'n', 'x', }, '<leader>ga',
-          package.loaded.gitsigns.stage_buffer,
+        vim.keymap.set({ 'n', 'x', }, '<leader>ga', package.loaded.gitsigns.stage_buffer,
           { buffer = buffer, desc = 'git add file', })
-        vim.keymap.set({ 'n', 'x', }, '<leader>gr',
-          package.loaded.gitsigns.reset_buffer_index,
+        vim.keymap.set({ 'n', 'x', }, '<leader>gr', package.loaded.gitsigns.reset_buffer_index,
           { buffer = buffer, desc = 'git reset file', })
-        vim.keymap.set({ 'n', 'x', }, '<leader>gc',
-          package.loaded.gitsigns.reset_buffer,
+        vim.keymap.set({ 'n', 'x', }, '<leader>gc', package.loaded.gitsigns.reset_buffer,
           { buffer = buffer, desc = 'git checkout -- file', })
-        vim.keymap.set({ 'n', 'x', }, '<leader>gb',
-          package.loaded.gitsigns.toggle_current_line_blame,
+        vim.keymap.set({ 'n', 'x', }, '<leader>gb', package.loaded.gitsigns.toggle_current_line_blame,
           { buffer = buffer, desc = 'git blame', })
-        vim.keymap.set({ 'n', 'x', }, '<leader>gd',
-          package.loaded.gitsigns.toggle_deleted,
+        vim.keymap.set({ 'n', 'x', }, '<leader>gd', package.loaded.gitsigns.toggle_deleted,
           { buffer = buffer, })
         vim.keymap.set({ 'n', 'x', }, '<C-g>',
-          package.loaded.gitsigns.next_hunk,
+          function() package.loaded.gitsigns.nav_hunk('next', { target = 'all' }) end,
           { buffer = buffer, desc = 'next hunk', })
         vim.keymap.set({ 'n', 'x', }, '<C-S-g>',
-          package.loaded.gitsigns.prev_hunk,
+          function() package.loaded.gitsigns.nav_hunk('prev', { target = 'all' }) end,
           { buffer = buffer, desc = 'prev hunk', })
       end,
 
       signs = {
-        add = {
-          -- hl = 'GitSignsAdd',
-          text = '+',
-          -- numhl = 'GitSignsAddNr',
-          -- linehl = 'GitSignsAddLn',
-        },
-        change = {
-          -- hl = 'GitSignsChange',
-          text = '~',
-          -- numhl = 'GitSignsChangeNr',
-          -- linehl = 'GitSignsChangeLn',
-        },
-        delete = {
-          -- hl = 'GitSignsDelete',
-          text = '_',
-          -- numhl = 'GitSignsDeleteNr',
-          -- linehl = 'GitSignsDeleteLn',
-        },
-        topdelete = {
-          -- hl = 'GitSignsDelete',
-          text = '‾',
-          -- numhl = 'GitSignsDeleteNr',
-          -- linehl = 'GitSignsDeleteLn',
-        },
-        changedelete = {
-          -- hl = 'GitSignsChange',
-          text = '~',
-          -- numhl = 'GitSignsChangeNr',
-          -- linehl = 'GitSignsChangeLn',
-        },
+        add = { text = '+', },
+        change = { text = '~', },
+        delete = { text = '_', },
+        topdelete = { text = '‾', },
+        changedelete = { text = '~', },
+      },
+
+      signs_staged = {
+        add = { text = '▒', },
+        change = { text = '░', },
+        delete = { text = '░', },
+        topdelete = { text = '░', },
+        changedelete = { text = '░', },
       },
     })
   end,
