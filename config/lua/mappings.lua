f6fa585e17380e463ff5d78b2b2310b5896f3170
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-09 21:54:22 +0100

    Update config/lua/mappings.lua

--- config/lua/mappings.lua
+++ config/lua/mappings.lua
@@ -17,7 +17,7 @@ vim.keymap.set('n',           'q',          require('functions').wipe_active_buf
 vim.keymap.set('x',           '<C-n>',      ':Norm ')
 -- vim.keymap.set('n',           '<Leader>g',  '<cmd>silent !google-chrome-stable %:p<CR>')
 -- vim.keymap.set('n',           'g<Space>',   '<cmd>silent s/\\s\\+\\%#\\s*\\|\\s*\\%#\\s\\+/ /g<CR><cmd>nohlsearch<CR>')
-vim.keymap.set('n',           'g<Space>',   '<cmd>silent %s/\\s\\+$//e<CR>')
+vim.keymap.set('x',           'g<Space>',   ':s/\\s\\+$//<CR>')
 -- vim.keymap.set({ 'x', 'n', }, '<Space>',    '<Enter>',                                                                  { remap = true, })
 vim.keymap.set({ 'x', 'n', }, '<Enter>',    '<Space>',                                                                  { remap = true, })
 vim.keymap.set({ 'x', 'n', }, '<Leader>q',  '<cmd>qa!<CR>')
