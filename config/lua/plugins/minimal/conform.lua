a22cc9360d0c1f26d6e7a6fe2180c19d067b2c99
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-24 10:46:09 +0100

    Update config/lua/plugins/minimal/conform.lua

--- config/lua/plugins/minimal/conform.lua
+++ config/lua/plugins/minimal/conform.lua
@@ -1,31 +1,33 @@
 local vim = vim
 
 return {
-  'stevearc/conform.nvim',
+	'stevearc/conform.nvim',
 
-  enabled = true,
+	enabled = true,
 
-  config = function()
-    require('conform').setup({
-      formatters_by_ft = {
-        sh = { 'shfmt', 'shellharden', },
-        zsh = { 'shfmt', 'shellharden', }, -- 'beautysh'
-        python = { 'isort', 'black', },
-        javascript = { 'prettier', },
-        typescript = { 'prettier', },
-        html = { 'prettier', },
-        typescriptreact = { 'prettier', },
-        go = { 'gofmt', },
-      },
+	config = function()
+		require('conform').setup({
+			formatters_by_ft = {
+				sh = { 'shfmt', 'shellharden' },
+				zsh = { 'shfmt', 'shellharden' }, -- 'beautysh'
+				python = { 'isort', 'black' },
+				javascript = { 'prettier' },
+				typescript = { 'prettier' },
+				html = { 'prettier' },
+				typescriptreact = { 'prettier' },
+				go = { 'gofmt' },
+				lua = { 'stylua' },
+			},
 
-      formatters = {
-        shfmt = {
-          prepend_args = { '--indent', '2', '--case-indent', '--language-dialect', 'posix', '--simplify', },
-        },
-      },
-    })
+			formatters = {
+				shfmt = {
+					prepend_args = { '--indent', '2', '--case-indent', '--language-dialect', 'posix', '--simplify' },
+				},
+			},
+		})
 
-    vim.keymap.set({ 'n', 'x', }, '<Leader>f',
-      function() require('conform').format({ async = true, lsp_fallback = true, }) end)
-  end,
+		vim.keymap.set({ 'n', 'x' }, '<Leader>f', function()
+			require('conform').format({ async = true, lsp_fallback = true })
+		end)
+	end,
 }
