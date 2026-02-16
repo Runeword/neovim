d9f21a204f088c4e49606259eb11669012e02669
Runeword <60324746+Runeword@users.noreply.github.com>
2026-02-16 22:10:34 +0100

    Update config/lua/functions.lua

--- config/lua/functions.lua
+++ config/lua/functions.lua
@@ -128,47 +128,6 @@ function M.move_to_non_empty_line(lines)
 	vim.fn.cursor(new_line, vim.fn.getline(new_line):find('%S') or 1)
 end
 
--------------------- Undo
-
-function M.undoAllChanges()
-	if #vim.fn.undotree().entries == 0 then
-		return
-	end
-
-	local output = vim.fn.execute('undo 0')
-
-	vim.notify('' .. output:gsub('^\n', ''), 'info', { icon = '󰕌', render = 'wrapped-compact', timeout = 1200 })
-end
-
-function M.redoAllChanges()
-	if #vim.fn.undotree().entries == 0 then
-		return
-	end
-
-	local output = vim.fn.execute('undo ' .. vim.fn.undotree().seq_last)
-
-	vim.notify('' .. output:gsub('^\n', ''), 'info', { icon = '󰑎', render = 'wrapped-compact', timeout = 1200 })
-end
-
-function M.deleteUndoTree()
-	if #vim.fn.undotree().entries == 0 then
-		return
-	end
-
-	local start = vim.fn.getpos("'[")
-	local finish = vim.fn.getpos("']")
-
-	local view = vim.fn.winsaveview()
-	vim.o.undoreload = 0
-	vim.cmd('edit')
-	vim.fn.winrestview(view)
-
-	vim.fn.setpos("'[", start)
-	vim.fn.setpos("']", finish)
-
-	vim.notify('Delete undo tree', 'info', { icon = '', render = 'wrapped-compact', timeout = 1200 })
-end
-
 -------------------- Edit
 
 -- When the line is empty, move the cursor to the beginning of the line
