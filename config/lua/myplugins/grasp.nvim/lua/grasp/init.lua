dcae6818e7a76d53facba88b89567eadb847f0e8
Runeword <60324746+Runeword@users.noreply.github.com>
2026-02-24 22:16:19 +0100

    Update config/lua/myplugins/grasp.nvim/lua/grasp/init.lua

--- config/lua/myplugins/grasp.nvim/lua/grasp/init.lua
+++ config/lua/myplugins/grasp.nvim/lua/grasp/init.lua
@@ -85,7 +85,9 @@ function M.select_treesitter_node_under_cursor()
   local cursor_col = cursor_pos[2]
 
   local success, ts_parser = pcall(ts.get_parser, current_buffer_id)
-  if not success then return end
+  if not success then
+    return
+  end
 
   local ts_tree = ts_parser:tree_for_range({ cursor_row, cursor_col, cursor_row, cursor_col })
 
@@ -105,10 +107,12 @@ end
 
 function M.move_to_next_treesitter_node()
   local current_buffer_id = api.nvim_get_current_buf()
-  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id, })
+  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id })
 
   local success, ts_parser = pcall(ts.get_parser, current_buffer_id)
-  if not success then return end
+  if not success then
+    return
+  end
 
   local ts_tree = ts_parser:parse()[1]
   local ts_lang = ts.language.get_lang(current_buffer_filetype)
@@ -119,12 +123,12 @@ function M.move_to_next_treesitter_node()
   local cursor_col = cursor_pos[2]
 
   for _, ts_node in ts_query:iter_captures(ts_tree:root(), current_buffer_id, cursor_row - 1) do
-    local ts_range = { ts_node:range(), }
+    local ts_range = { ts_node:range() }
     local row_start = ts_range[1] + 1
     local col_start = ts_range[2]
 
     if row_start > cursor_row or (row_start == cursor_row and col_start > cursor_col) then
-      api.nvim_win_set_cursor(0, { row_start, col_start, })
+      api.nvim_win_set_cursor(0, { row_start, col_start })
       break
     end
   end
@@ -132,10 +136,12 @@ end
 
 function M.move_to_prev_treesitter_node()
   local current_buffer_id = api.nvim_get_current_buf()
-  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id, })
+  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id })
 
   local success, ts_parser = pcall(ts.get_parser, current_buffer_id)
-  if not success then return end
+  if not success then
+    return
+  end
 
   local ts_tree = ts_parser:parse()[1]
   local ts_lang = ts.language.get_lang(current_buffer_filetype)
@@ -151,12 +157,12 @@ function M.move_to_prev_treesitter_node()
   end
 
   for i = 1, #ts_nodes do
-    local ts_range = { ts_nodes[i]:range(), }
+    local ts_range = { ts_nodes[i]:range() }
     local row_start = ts_range[1] + 1
     local col_start = ts_range[2]
 
     if row_start < cursor_row or (row_start == cursor_row and col_start < cursor_col) then
-      api.nvim_win_set_cursor(0, { row_start, col_start, })
+      api.nvim_win_set_cursor(0, { row_start, col_start })
       break
     end
   end
@@ -167,27 +173,18 @@ local namespace_id = api.nvim_create_namespace('TreesitterObjectHighlight')
 local function highlight_treesitter_node()
   api.nvim_buf_clear_namespace(0, namespace_id, 0, -1)
 
-  if api.nvim_get_mode().mode ~= 'n' then return end
+  if api.nvim_get_mode().mode ~= 'n' then
+    return
+  end
 
-  local current_buffer_id = api.nvim_get_current_buf()
+  local ts_node = ts.get_node({ ignore_injections = false })
+  if not ts_node then
+    return
+  end
 
-  local cursor_pos = api.nvim_win_get_cursor(0)
-  local cursor_row = cursor_pos[1] - 1 -- API uses 0-based rows
-  local cursor_col = cursor_pos[2]
-
-  local success, ts_parser = pcall(ts.get_parser, current_buffer_id)
-  if not success then return end
-
-  local ts_tree = ts_parser:parse()[1]
-
-  -- Get the node at the cursor position
-  local ts_node = ts_tree:root():named_descendant_for_range(cursor_row, cursor_col, cursor_row, cursor_col)
-
-  -- Get the range of the node
   local start_row, start_col, end_row, end_col = ts_node:range()
 
-  -- Highlight the node
-  api.nvim_buf_set_extmark(current_buffer_id, namespace_id, start_row, start_col, {
+  api.nvim_buf_set_extmark(0, namespace_id, start_row, start_col, {
     end_row = end_row,
     end_col = end_col,
     hl_group = 'TreesitterObjectHighlight',
@@ -195,7 +192,7 @@ local function highlight_treesitter_node()
 end
 
 api.nvim_create_autocmd({ 'CursorMoved', 'ModeChanged' }, {
-  group = api.nvim_create_augroup('TreesitterObjectHighlight', { clear = true, }),
+  group = api.nvim_create_augroup('TreesitterObjectHighlight', { clear = true }),
   callback = highlight_treesitter_node,
 })
 
