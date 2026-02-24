63bec6885ec1a362a3dedd32bf906496d1e50ca6
Runeword <60324746+Runeword@users.noreply.github.com>
2026-02-24 22:27:45 +0100

    Update config/lua/plugins/search/grugfar.lua

--- config/lua/plugins/search/grugfar.lua
+++ config/lua/plugins/search/grugfar.lua
@@ -2,15 +2,16 @@ local vim = vim
 
 return {
   'MagicDuck/grug-far.nvim',
+  enabled = false,
 
   config = function()
     local grugFar = require('grug-far')
 
-    grugFar.setup({});
+    grugFar.setup({})
 
     vim.keymap.set('n', 'R', function()
       local current_word = vim.fn.expand('<cword>')
-      grugFar.open({ prefills = { search = current_word, }, })
-    end, { desc = 'Open grug-far with current word', })
+      grugFar.open({ prefills = { search = current_word } })
+    end, { desc = 'Open grug-far with current word' })
   end,
 }
