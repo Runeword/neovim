9b23c2c6740eb43d00118d30cddf81977a9f7bbc
Runeword <60324746+Runeword@users.noreply.github.com>
2026-02-24 22:00:26 +0100

    Update config/lua/plugins/minimal/spider.lua

--- config/lua/plugins/minimal/spider.lua
+++ config/lua/plugins/minimal/spider.lua
@@ -6,8 +6,7 @@ return {
   'chrisgrieser/nvim-spider',
 
   config = function()
-    vim.keymap.set({ 'n', 'x', }, 'W',
-    function()
+    vim.keymap.set({ 'n', 'x' }, 'W', function()
       require('spider').motion('w', {
         customPatterns = {
           -- Match one or more characters that are neither word characters nor whitespace characters
@@ -16,37 +15,35 @@ return {
       })
     end, {})
 
-    vim.keymap.set({ 'n', 'x', }, 'w',
-      function()
-        require('spider').motion('w', {
-          customPatterns = {
-            -- subwordPatterns
-            '[%w\128-\255]+',
-            '%u%l+',
-            -- skipPunctuationPatterns
-            '%f[^%s]%p+%f[%s]',
-            '^%p+%f[%s]',
-            '%f[^%s]%p+$',
-            '^%p+$',
-          },
-        })
-      end, { desc = 'Spider-w', })
+    vim.keymap.set({ 'n', 'x' }, 'w', function()
+      require('spider').motion('w', {
+        customPatterns = {
+          -- subwordPatterns
+          '[%w\128-\255]+',
+          '%u%l+',
+          -- skipPunctuationPatterns
+          '%f[^%s]%p+%f[%s]',
+          '^%p+%f[%s]',
+          '%f[^%s]%p+$',
+          '^%p+$',
+        },
+      })
+    end, { desc = 'Spider-w' })
 
-    vim.keymap.set({ 'n', 'x', }, 'b',
-      function()
-        require('spider').motion('b', {
-          customPatterns = {
-            -- subwordPatterns
-            '[%w\128-\255]+',
-            '%l%u+',
-            -- skipPunctuationPatterns
-            '%f[^%s]%p+%f[%s]',
-            '^%p+%f[%s]',
-            '%f[^%s]%p+$',
-            '^%p+$',
-          },
-        })
-      end, { desc = 'Spider-b', })
+    vim.keymap.set({ 'n', 'x' }, 'b', function()
+      require('spider').motion('b', {
+        customPatterns = {
+          -- subwordPatterns
+          '[%w\128-\255]+',
+          '%l%u+',
+          -- skipPunctuationPatterns
+          '%f[^%s]%p+%f[%s]',
+          '^%p+%f[%s]',
+          '%f[^%s]%p+$',
+          '^%p+$',
+        },
+      })
+    end, { desc = 'Spider-b' })
 
     local subwordPatterns = {
       '[%w\128-\255]+',
@@ -56,14 +53,14 @@ return {
       '^%p+$',
     }
 
-    vim.keymap.set({ 'n', 'x', 'o', }, 'e',
-      function() require('spider').motion('e', { customPatterns = subwordPatterns }) end,
-      { desc = 'Spider-e', })
-    vim.keymap.set({ 'n', 'x', 'o', }, 'ge',
-      function() require('spider').motion('ge', { customPatterns = subwordPatterns }) end,
-      { desc = 'Spider-ge', })
+    vim.keymap.set({ 'n', 'x', 'o' }, 'e', function()
+      require('spider').motion('e', { customPatterns = subwordPatterns })
+    end, { desc = 'Spider-e' })
+    vim.keymap.set({ 'n', 'x', 'o' }, 'ge', function()
+      require('spider').motion('ge', { customPatterns = subwordPatterns })
+    end, { desc = 'Spider-ge' })
 
-    vim.keymap.set({ 'o', }, 'w', 'iw')
-    vim.keymap.set({ 'o', }, 'W', 'iW')
+    vim.keymap.set({ 'o' }, 'w', 'iw')
+    vim.keymap.set({ 'o' }, 'W', 'iW')
   end,
 }
