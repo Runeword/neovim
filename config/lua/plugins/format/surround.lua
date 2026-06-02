7022b370b5381226f48046d484e71d1f575ec20e
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-02 23:39:29 +0200

    Update config/lua/plugins/format/surround.lua

--- config/lua/plugins/format/surround.lua
+++ config/lua/plugins/format/surround.lua
@@ -2,25 +2,16 @@ return {
   'kylechui/nvim-surround',
   enabled = true,
 
+  init = function()
+    vim.g.nvim_surround_no_mappings = true
+  end,
+
   config = function()
-    require('nvim-surround').setup({
-      keymaps = {
-        insert = false,
-        insert_line = false,
-        normal_cur = false,
-        normal_line = false,
-        normal_cur_line = false,
-        visual_line = false,
-        -- change_line = false,
-        -- normal = false,
-        -- delete = false,
-        -- change = false,
-        -- visual = false,
-        normal = 's',
-        delete = 'ds',
-        change = 'cs',
-        visual = 's',
-      },
-    })
+    require('nvim-surround').setup({})
+    vim.keymap.set('n', 's', '<Plug>(nvim-surround-normal)')
+    vim.keymap.set('n', 'ds', '<Plug>(nvim-surround-delete)')
+    vim.keymap.set('n', 'cs', '<Plug>(nvim-surround-change)')
+    vim.keymap.set('n', 'cS', '<Plug>(nvim-surround-change-line)')
+    vim.keymap.set('x', 's', '<Plug>(nvim-surround-visual)')
   end,
 }
