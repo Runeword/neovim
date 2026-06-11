1558b1b7b9af3146b17277301a974d112fc329ef
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-11 23:41:33 +0200

    Update config/lua/plugins/minimal/undotree.lua

--- config/lua/plugins/minimal/undotree.lua
+++ config/lua/plugins/minimal/undotree.lua
@@ -6,6 +6,7 @@ return {
   dir = vim.fn.stdpath('config') .. '/lua/myplugins/undotree.nvim',
 
   config = function()
+    require('undotree').setup()
     vim.keymap.set('n', 'U', require('undotree').undoAllChanges, { desc = 'Undo all changes' })
     vim.keymap.set('n', 'R', require('undotree').redoAllChanges, { desc = 'Redo all changes' })
     vim.keymap.set('n', '<Leader>s', require('undotree').deleteUndoTree, { desc = 'Delete undo tree' })
