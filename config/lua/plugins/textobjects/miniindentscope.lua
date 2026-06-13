956d846b6003c673518d912904920d130dbdd70a
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-13 19:38:48 +0200

    Update config/lua/plugins/textobjects/miniindentscope.lua

--- config/lua/plugins/textobjects/miniindentscope.lua
+++ config/lua/plugins/textobjects/miniindentscope.lua
@@ -5,26 +5,13 @@ return {
 
   version = false,
 
-  init = function()
-    vim.api.nvim_create_augroup('indentscope', { clear = true, })
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      group = 'indentscope',
-      pattern = '*',
-      callback = function()
-        vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol',    { bg = 'none', fg = '#222b66', })
-        vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbolOff', { bg = 'none', fg = '#222b66', })
-      end,
-    })
-  end,
-
   config = function()
-    vim.api.nvim_create_augroup('indentscope', { clear = true, })
+    vim.api.nvim_create_augroup('indentscope', { clear = true })
 
     vim.api.nvim_create_autocmd('FileType', {
       group = 'indentscope',
       pattern = '*',
-      command =
-      "if index(['help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'NvimTree', 'neo-tree', 'Trouble'], &ft) != -1 || index(['nofile', 'terminal', 'lsp-installer', 'lspinfo'], &bt) != -1 | let b:miniindentscope_disable=v:true | endif",
+      command = "if index(['help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'NvimTree', 'neo-tree', 'Trouble'], &ft) != -1 || index(['nofile', 'terminal', 'lsp-installer', 'lspinfo'], &bt) != -1 | let b:miniindentscope_disable=v:true | endif",
     })
 
     local scope = require('mini.indentscope')
