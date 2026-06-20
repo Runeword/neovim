320b70255ea7ddd13f96cd961e35ba9b341ac62e
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-21 00:19:46 +0200

    Update config/lua/plugins/minimal/fzflua.lua

--- config/lua/plugins/minimal/fzflua.lua
+++ config/lua/plugins/minimal/fzflua.lua
@@ -4,25 +4,62 @@ return {
   'ibhagwan/fzf-lua',
   enabled = true,
 
-  config = function()
-    vim.keymap.set('n', '<Leader><Tab>',        require('fzf-lua').files)
-    vim.keymap.set('n', '<Leader><CR>', require('fzf-lua').live_grep_resume)
-    vim.keymap.set('n', '<Leader>h',    require('fzf-lua').help_tags)
-    vim.keymap.set('n', '<Leader>k',    require('fzf-lua').keymaps)
-    vim.keymap.set('n', '<Leader>i',    require('fzf-lua').highlights)
-    vim.keymap.set({ 'n', 'x', }, '<leader>a', function()
-      require('fzf-lua').lsp_code_actions {
-        winopts = {
-          relative = 'cursor',
-          width = 0.6,
-          height = 0.6,
-          row = 1,
-          preview = { vertical = 'up:70%', },
-        },
-      }
-    end)
-    -- vim.keymap.set("n", "<Leader>x", "<cmd>lua require('fzf-lua').quickfix({multiprocess=true})<CR>")
+  keys = {
+    {
+      '<Leader><Tab>',
+      function()
+        require('fzf-lua').files()
+      end,
+      desc = 'fzf: files',
+    },
+    {
+      '<Leader><CR>',
+      function()
+        require('fzf-lua').live_grep_resume()
+      end,
+      desc = 'fzf: live grep',
+    },
+    {
+      '<Leader>h',
+      function()
+        require('fzf-lua').help_tags()
+      end,
+      desc = 'fzf: help tags',
+    },
+    {
+      '<Leader>k',
+      function()
+        require('fzf-lua').keymaps()
+      end,
+      desc = 'fzf: keymaps',
+    },
+    {
+      '<Leader>i',
+      function()
+        require('fzf-lua').highlights()
+      end,
+      desc = 'fzf: highlights',
+    },
+    {
+      '<leader>a',
+      mode = { 'n', 'x' },
+      function()
+        require('fzf-lua').lsp_code_actions({
+          winopts = {
+            relative = 'cursor',
+            width = 0.6,
+            height = 0.6,
+            row = 1,
+            preview = { vertical = 'up:70%' },
+          },
+        })
+      end,
+      desc = 'fzf: code actions',
+    },
+    -- { '<Leader>x', function() require('fzf-lua').quickfix({ multiprocess = true }) end, },
+  },
 
+  config = function()
     local actions = require('fzf-lua.actions')
 
     require('fzf-lua').setup({
@@ -35,7 +72,7 @@ return {
           horizontal = 'up:70%',
           title = false,
           delay = 0,
-          scrollchars = { '▎', '', },
+          scrollchars = { '▎', '' },
         },
       },
 
@@ -56,8 +93,7 @@ return {
       lsp = {
         code_actions = {
           previewer = 'codeaction_native',
-          preview_pager =
-          "delta --side-by-side --width=$FZF_PREVIEW_COLUMNS --hunk-header-style='omit' --file-style='omit'",
+          preview_pager = "delta --side-by-side --width=$FZF_PREVIEW_COLUMNS --hunk-header-style='omit' --file-style='omit'",
         },
       },
 
