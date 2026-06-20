105514786e8c401ce5c1f03b0d4b5555606dba24
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-21 00:30:20 +0200

    Update config/lua/options.lua

--- config/lua/options.lua
+++ config/lua/options.lua
@@ -23,28 +23,26 @@ o.completeopt = 'menuone,noinsert' -- Options for Insert mode completion
 o.pumblend = 10
 -- o.pumwidth = 40
 -- o.pumheight = 15
-o.clipboard =
-'unnamedplus'          -- Have the clipboard be the same as my regular clipboard
-o.updatetime = 50      -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience
+o.clipboard = 'unnamedplus' -- Have the clipboard be the same as my regular clipboard
+o.updatetime = 50 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience
 o.swapfile = false
 o.termguicolors = true -- Enables 24-bit RGB color in the Terminal UI
-o.showmode = false     -- Disable message on the last line (Insert, Replace or Visual mode)
-o.linebreak = true     -- Do not break words on line wrap
-o.breakindent = true   -- Start wrapped lines indented
-o.ignorecase = true    -- Ignore case in search patterns
-o.smartcase = true     -- Override the 'ignorecase' option if the search pattern contains upper case characters
+o.showmode = false -- Disable message on the last line (Insert, Replace or Visual mode)
+o.linebreak = true -- Do not break words on line wrap
+o.breakindent = true -- Start wrapped lines indented
+o.ignorecase = true -- Ignore case in search patterns
+o.smartcase = true -- Override the 'ignorecase' option if the search pattern contains upper case characters
 o.signcolumn = 'yes:1'
-o.expandtab = true     -- Use the appropriate number of spaces to insert a <Tab>
-o.smartindent = true   -- Do smart autoindenting when starting a new line
-o.copyindent = true    -- Copy the structure of the existing lines indent when autoindenting a new line
-o.softtabstop = 2      -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
-o.tabstop = 2          -- Number of spaces that a <Tab> in the file counts for
-o.shiftwidth = 2       -- Number of spaces to use for each step of (auto)indent
-o.hidden = true        -- Allow switching buffers with unsaved changes
-opt.lazyredraw = true  -- When running macros and regexes on a large file, lazy redraw tells neovim/vim not to draw the screen, which greatly speeds it up, upto 6-7x faster
-opt.viminfofile = vim.fn.stdpath("cache") .. "/viminfo" -- Set viminfofile to XDG cache directory
+o.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>
+o.smartindent = true -- Do smart autoindenting when starting a new line
+o.copyindent = true -- Copy the structure of the existing lines indent when autoindenting a new line
+o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
+o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+o.hidden = true -- Allow switching buffers with unsaved changes
+opt.viminfofile = vim.fn.stdpath('cache') .. '/viminfo' -- Set viminfofile to XDG cache directory
 opt.list = true
-opt.listchars:append 'eol:¬,nbsp:•,tab:  ,lead: ,multispace:˙,trail:˙'
+opt.listchars:append('eol:¬,nbsp:•,tab:  ,lead: ,multispace:˙,trail:˙')
 opt.laststatus = 3
 opt.showcmd = false
 -- opt.spell = true
