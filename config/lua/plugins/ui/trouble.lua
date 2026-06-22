29408e74a3ea133f95522d5a186b3e09baa4eb12
Runeword <60324746+Runeword@users.noreply.github.com>
2026-06-22 23:23:20 +0200

    Update config/lua/plugins/ui/trouble.lua

--- config/lua/plugins/ui/trouble.lua
+++ config/lua/plugins/ui/trouble.lua
@@ -3,6 +3,9 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   cmd = 'Trouble',
   opts = {
+    keys = {
+      ['<esc>'] = 'close',
+    },
     win = {
       wo = {
         winhighlight = 'Normal:TroubleNormal,NormalNC:TroubleNormalNC,EndOfBuffer:TroubleNormal,CursorLine:TroubleCursorLine',
